5d6e481a03fbc680977ead89ef1022acc92937a7
Jonika <katyadyduch@gmail.com>
2024-02-18 22:16:27 +0200

    first commit

---
+++ database/migrations/2024_02_18_193859_create_database.php
@@ -0,0 +1,103 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();;
+            $table->foreign('parent_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('color', 255);
+            $table->unsignedBigInteger('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->text('description')->nullable();
+            $table->enum('sex', ['woman', 'man', 'unisex']);
+            $table->float('price', 8, 2);
+            $table->timestamps();
+        });
+
+        Schema::create('bannes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->text('reason')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->unsignedBigInteger('item_id');
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->timestamps();
+        });
+
+        Schema::create('history_orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('item_id');
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('item_id');
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        Schema::create('basket', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('item_id');
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('basket');
+        Schema::dropIfExists('likes');
+        Schema::dropIfExists('history_orders');
+        Schema::dropIfExists('images');
+        Schema::dropIfExists('bannes');
+        Schema::dropIfExists('items');
+        Schema::dropIfExists('brands');
+        Schema::dropIfExists('categories');
+    }
+};
