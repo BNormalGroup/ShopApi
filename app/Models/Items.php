9f68fdb4990e888d3f9137059795127fff15b150
Jonika <katyadyduch@gmail.com>
2024-04-23 23:45:25 +0300

    add texture column

--- app/Models/Items.php
+++ app/Models/Items.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Items extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'description', 'price', 'brand_id', 'category_id', 'sex', 'image'];
+    protected $fillable = ['name', 'description', 'price', 'brand_id', 'category_id', 'sex', 'image', 'texture'];
     public function likes()
     {
         return $this->hasMany('App\Models\Likes', 'item_id');
