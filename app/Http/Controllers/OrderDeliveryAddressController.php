417c862f7de8326fed4cb12f395de1892510ed74
Jonika <katyadyduch@gmail.com>
2024-05-19 14:13:41 +0300

    order table

---
+++ app/Http/Controllers/OrderDeliveryAddressController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderDeliveryAddress;
+use Illuminate\Http\Request;
+
+class OrderDeliveryAddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OrderDeliveryAddress $orderDeliveryAddress)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OrderDeliveryAddress $orderDeliveryAddress)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OrderDeliveryAddress $orderDeliveryAddress)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OrderDeliveryAddress $orderDeliveryAddress)
+    {
+        //
+    }
+}
