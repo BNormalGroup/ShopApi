7a581211be9cf08c8929bd043b7c7f422a315e48
Jonika <katyadyduch@gmail.com>
2024-05-25 15:20:49 +0300

    add middleware basket

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class BasketController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     public function store(Request $request)
     {
         $basket = Basket::create([
